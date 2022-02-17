232833a78e1e5cbbbda7ed22e01a16ce6f3cd000
Ojochilemi Okoka <okokaojo@outlook.com>
2022-02-16 22:20:26 -0500

    Initial Commit

---
+++ Project 3/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Project 3
+//
+//  Created by Ojochilemi Okoka on 2/16/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
